62b5f13e6385a2225c9fce3df9eb8ed50d9a7b15
Ruben <ruben@bokobza.info>
2023-06-29 19:23:52 +0300

    Debug

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 from app import app
-from waitress import serve
-serve(app, host='0.0.0.0', port=5000, url_scheme='https')
+
+debug = True
+
+if debug:
+    app.run(debug=True, port=5000)
+else:
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
