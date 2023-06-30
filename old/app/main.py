ed796fafad2f93769c9286324321b11f403d5483
Ruben <ruben@bokobza.info>
2023-06-30 07:46:41 +0300

    Reset

--- old/app/main.py
+++
@@ -1,9 +0,0 @@
-from app import app
-
-debug = True
-
-if debug:
-    app.run(debug=True, host='0.0.0.0', port=5000)
-else:
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
